2ea5cfeb67ebabef63fd402f9b9ce8f9697565f6
Ahmed Rabii Zaiyet <ahmed.rabii.zaiyet@gmail.com>
2023-04-24 19:27:59 +0200

    change error message

--- src/components/UI/ErrorUI.vue
+++ src/components/UI/ErrorUI.vue
@@ -4,7 +4,7 @@
   <div class="card">
     <div class="card-body">
       <div class="alert alert-danger" role="alert">
-        Problèmes de connexion, veuillez contacter votre administrateur
+        Problèmes de connexion, veuillez contacter votre administrateur.
       </div>
     </div>
   </div>
