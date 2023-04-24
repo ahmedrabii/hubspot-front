2d077655743f935424e141fe8be283a675de96d2
Ahmed Rabii Zaiyet <ahmed.rabii.zaiyet@gmail.com>
2023-04-24 19:17:58 +0200

    change assets loading

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -17,7 +17,7 @@ import * as fs from 'fs'
     console.log('Pushing to gh-pages...')
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
     await execa('rm', ['-r', folderName])
-    await execa('git', ['checkout', '-f', 'main'])
+    await execa('git', ['checkout', '-f', 'master'])
     await execa('git', ['branch', '-D', 'gh-pages'])
     console.log('Successfully deployed, check your settings')
   } catch (e) {
